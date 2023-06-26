d39eabcaba5e1832061b8d99d7f134d83fe4b03b
肖润杰 <15014586591@139.com>
2023-06-26 22:50:16 +0800

    配置app免鉴权，开启数据库操作记录

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/shiro/ShiroConfig.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/shiro/ShiroConfig.java
@@ -80,6 +80,11 @@ public class ShiroConfig {
             }
         }
         // 配置不会被拦截的链接 顺序判断
+
+
+//        App端
+        filterChainDefinitionMap.put("/app/**", "anon");
+
         filterChainDefinitionMap.put("/sys/cas/client/validateLogin", "anon"); //cas验证登录
         filterChainDefinitionMap.put("/sys/randomImage/**", "anon"); //登录验证码接口排除
         filterChainDefinitionMap.put("/sys/checkCaptcha", "anon"); //登录验证码接口排除
