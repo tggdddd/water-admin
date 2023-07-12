4457d0fb93e71f48c36ad173c9d02de357164a02
肖润杰 <15014586591@139.com>
2023-07-12 17:36:06 +0800

    加功能。。。。

--- jeecg-module-demo/src/main/java/org/jeecg/modules/demo/water/appController/ShareController.java
+++ jeecg-module-demo/src/main/java/org/jeecg/modules/demo/water/appController/ShareController.java
@@ -1,13 +1,18 @@
 package org.jeecg.modules.demo.water.appController;
 
 import com.alibaba.fastjson.JSONObject;
+import org.jeecg.common.system.util.JwtUtil;
 import org.jeecg.common.util.RestUtil;
 import org.jeecg.config.thirdapp.ThirdAppConfig;
-import org.jeecg.modules.base.ThinkResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 
 @RestController
 @RequestMapping("/app/share")
@@ -15,26 +20,50 @@ public class ShareController {
     @Autowired
     ThirdAppConfig thirdAppConfig;
 
+    /**
+     * 推广分享
+     */
+    @RequestMapping("/promote")
+    public byte[] shareToNew(HttpServletRequest request) {
+        String username = JwtUtil.getUserNameByToken(request);
+        String sceneData = "username=" + username;
+        String token = getAccessToken();
+        JSONObject params = new JSONObject();
+        params.put("scene", sceneData);
+        String url = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=" + token;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(params.toJSONString().length()));
+        return RestUtil.request(url, HttpMethod.POST, headers, null, params, byte[].class).getBody();
+    }
+
+    /**
+     * 商品链接分享
+     */
     @RequestMapping("/get")
-    public ThinkResult share(@RequestParam("shopId") String ShopId) {
+    public byte[] share(@RequestParam("shopId") String ShopId) {
         String page = "pages/goods/goods";
-        String sceneData = ShopId; //抽奖id ？？？？
+        String token = getAccessToken();
+        JSONObject params = new JSONObject();
+        params.put("scene", ShopId);
+        params.put("page", page);
+        params.put("width", 200);
+        params.put("check_path", "false");
+        String url = "https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=" + token;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(HttpHeaders.CONTENT_LENGTH, String.valueOf(params.toJSONString().length()));
+        byte[] body = RestUtil.request(url, HttpMethod.POST, headers, null, params, byte[].class).getBody();
+        return Base64.getMimeEncoder().encode(body);
+    }
+
+    /**
+     * 获取token
+     */
+    private String getAccessToken() {
         String url = "https://api.weixin.qq.com/cgi-bin/token" +
                 "?grant_type=client_credential" +
                 "&secret=" + thirdAppConfig.getWechatSmall().getClientSecret() +
                 "&appid=" + thirdAppConfig.getWechatSmall().getClientId();
-
         JSONObject post = RestUtil.get(url);
-        String token = post.getString("access_token");
-        JSONObject params = new JSONObject();
-        params.put("scene", sceneData);
-        params.put("page", page);
-        params.put("width", 200);
-        JSONObject result = RestUtil.post("https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=" + token, params);
-        if (result.get("errcode").equals("0")) {
-            Object o = result.get("buffer");
-            return ThinkResult.ok(o);
-        }
-        return ThinkResult.error();
+        return post.getString("access_token");
     }
 }
